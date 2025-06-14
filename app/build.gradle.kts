db1eea560130f561ba9b902d81dcc200335842fc
Erick Larrea <20200023@ue.edu.pe>
2025-06-14 18:23:58 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("kotlin-kapt")
+
+
+
+
+
+}
+
+
+android {
+    namespace = "dev.larrea.pc2_larrea"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "dev.larrea.pc2_larrea"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    //Add navigation compose
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    //Add coil compose
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    //Add room
+    implementation("androidx.room:room-runtime:2.6.0")
+    //Add room ktx
+    implementation("androidx.room:room-ktx:2.6.0")
+    //Add room compiler
+    kapt("androidx.room:room-compiler:2.6.0")
+    //Add Material icons
+    implementation("androidx.compose.material:material-icons-extended")
+    //Add coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    //Add lifecycle viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+}
